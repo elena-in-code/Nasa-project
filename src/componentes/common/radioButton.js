1288f7ab4bd2d6e33dd7abe9d20b33be1237505b
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2020-04-20 17:51:58 +0200

    add RadioButton

---
+++ src/componentes/common/radioButton.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './radioButton.scss';
+
+const RadioButton = (props) => {
+  const { checked, onChange } = props;
+
+  return (
+    <div className="radio-button__wrapper">
+      <div className="radio-button__section">
+        <input
+          className="radio-button__input"
+          type="radio"
+          id="all"
+          name="mediaRadioButtons"
+          value="all"
+          checked={checked === 'all'}
+          onChange={onChange}
+        />
+        <label htmlFor="all">All</label>
+      </div>
+
+      <div className="radio-button__section">
+        <input
+          className="radio-button__input"
+          type="radio"
+          id="images"
+          name="mediaRadioButtons"
+          value="image"
+          checked={checked === 'image'}
+          onChange={onChange}
+        />
+        <label htmlFor="images">Images</label>
+      </div>
+
+      <div className="radio-button__section">
+        <input
+          className="radio-button__input"
+          type="radio"
+          id="videos"
+          name="mediaRadioButtons"
+          value="video"
+          checked={checked === 'video'}
+          onChange={onChange}
+        />
+        <label htmlFor="videos">Videos</label>
+      </div>
+    </div>
+  );
+};
+
+RadioButton.propTypes = {
+  checked: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+RadioButton.defaultProps = {
+  checked: 'all',
+  onChange: () => {},
+};
+
+export default RadioButton;
--- src/componentes/search/search.js
+++ src/componentes/search/search.js
@@ -4,6 +4,7 @@ import Form from '../common/form';
 import Card from '../common/card';
 import SearchTerm from '../common/searchTerm';
 import Message from '../common/message';
+import RadioButton from '../common/radioButton';
 
 import './search.scss';
 
@@ -15,63 +16,103 @@ const Search = () => {
   const [showInput, setShowInput] = useState(true);
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [radioButtonSelection, setRadioButtonSelection] = useState('all');
 
-  const handleChange = event => {
+  const mediaTypeURL =
+    radioButtonSelection !== 'all' && `&media_type=${radioButtonSelection}`;
+
+  const handleChange = (event) => {
     setInputValue(event.target.value);
     setSubmitted(false);
   };
 
   const handleSubmit = () => {
-    if(inputValue){
-      setLoading(true)
-      fetch(nasaURL+inputValue)
-        .then(response =>response.json()
-        .then(data => {
-          setItems(data.collection.items)
-          setLoading(false)
-        })
-        .catch((error) => console.log(error))
-        );
+    if (inputValue) {
+      setLoading(true);
+      fetch(nasaURL + inputValue + mediaTypeURL).then((response) =>
+        response
+          .json()
+          .then((data) => {
+            setItems(data.collection.items);
+            setLoading(false);
+          })
+          .catch((error) => console.log(error))
+      );
     }
     return setSubmitted(true);
   };
 
   const renderMessage = () => {
-    if(loading) return <Message messageText="Loading..." messageStatusClassName="info"/> 
-    if (submitted && inputValue && items.length === 0) return <Message messageText="There is no results for your search, please reset your search and try again" messageStatusClassName="warning"/>
-    if(!inputValue) return <Message messageText="Please insert text to start searching" messageStatusClassName="info"/>
-  }
+    if (loading)
+      return <Message messageText="Loading..." messageStatusClassName="info" />;
+    if (submitted && inputValue && items.length === 0)
+      return (
+        <Message
+          messageText="There is no results for your search, please reset your search and try again"
+          messageStatusClassName="warning"
+        />
+      );
+    if (!inputValue)
+      return (
+        <Message
+          messageText="Please insert text to start searching"
+          messageStatusClassName="info"
+        />
+      );
+  };
 
   const handleReset = () => {
     setItems([]);
     setInputValue('');
     setShowInput(true);
     setSubmitted(false);
-  }
-  
+  };
+
   const renderSearchResults = () => {
-    if (items){
-        const getCardComponent = items.map(item => {
-          const { data, links } = item;
-          const { title, description, nasa_id } = data[0];
-          return <Card key={nasa_id} cardTitle={title} image={links ? links[0].href : ''} imageAlt={title} description={description} />;
-        }); 
+    if (items) {
+      const getCardComponent = items.map((item) => {
+        const { data, links } = item;
+        const { title, description, nasa_id } = data[0];
+        return (
+          <Card
+            key={nasa_id}
+            cardTitle={title}
+            image={links ? links[0].href : ''}
+            imageAlt={title}
+            description={description}
+          />
+        );
+      });
       return getCardComponent;
-    } 
+    }
+  };
+
+  const handleRadioButtonChange = (event) => {
+    if (event && event.target.checked) {
+      setRadioButtonSelection(event.target.value);
+    }
   };
 
   return (
-    <div className="search__container">   
-    <Form 
-        onClick={items.length === 0 ? () => handleSubmit(): () => handleReset()} 
-        onChange={handleChange} 
-        value={inputValue} 
-        buttonText={items.length === 0 ? "Search" : "Reset Search"}
-        showInput = {items.length === 0 && showInput}
+    <div className="search__container">
+      <Form
+        onClick={
+          items.length === 0 ? () => handleSubmit() : () => handleReset()
+        }
+        onChange={handleChange}
+        value={inputValue}
+        buttonText={items.length === 0 ? 'Search' : 'Reset Search'}
+        showInput={items.length === 0 && showInput}
       />
+      {items.length === 0 && (
+        <RadioButton
+          checked={radioButtonSelection}
+          onChange={handleRadioButtonChange}
+        />
+      )}
       {renderMessage()}
       {items.length !== 0 && <SearchTerm searchTerm={inputValue} />}
-      <div className='search-results__container'>
+      <div className="search-results__container">
         {items.length !== 0 && renderSearchResults()}
       </div>
     </div>
